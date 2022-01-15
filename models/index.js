27ab51266f11efb97a1ce40239b6d010c4192f14
X <xac923@outlook.com>
2022-01-15 00:39:25 -0800

    created keyboard schema

---
+++ models/index.js
@@ -0,0 +1,13 @@
+//Connect the db to MongoDBAtlas
+const mongoose = require('mongoose')
+const db = mongoose.connection;
+const dbUrl = process.env.DATABASE_URL;
+
+mongoose
+	.connect(dbUrl)
+	.then(() =>
+		console.log(
+			`MongoDB successfully connected at ${db.host}:${db.port}! How dope!`
+		)
+	)
+	.catch((err) => console.log(`MongoDB connection FAILED :( Error: ${err}`));
---
+++ models/keyboard.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const keyboardSchema = new mongoose.Schema(
+    {
+    keyboard_name: {
+        type: String,
+        required: [true, "You must provide a name for your board"],
+        },
+    size: {
+        type: String,
+        required: [true, "You must provide a size type for your board"],
+        },
+    plate_material: {
+        type: String,
+        required: [true, "You must provide a material type for your board"],
+    },
+    switch_type: {
+        type: String,
+        required: [true, "You must provide a switch type for your board"],
+    },
+    PCB_type: {
+        type: String,
+        required: [true, "You must provide a type of circuit for your board"],
+    },
+    oil_type: {
+        type: String, 
+        required: [true, "You must provide a type of oil for the board"],
+    },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+module.exports = mongoose.model("Keyboards", keyboardSchema);

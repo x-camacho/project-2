88449ea8885f3235849d4f116f602455bbf9e1d3
X <xac923@outlook.com>
2022-01-15 00:23:40 -0800

    added in additional modules and directories for organization

---
+++ server.js
@@ -0,0 +1,17 @@
+// External Modules //
+const express = require('express');
+
+// Instanced Module //
+const app = express();
+
+// Configuration //
+const PORT = process.env.PORT || 4000; //for deployment in heroku
+app.set("view engine", "ejs");
+
+// Internal Routes //
+app.get('/', (req, res) => { //Renders Landing Page
+    res.render("index");
+});
+
+// Server Listener //
+app.listen(PORT, () => console.log(`YO! Server is connected at ${PORT}`))

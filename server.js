e18d6df5e802b38a2f41af59359f556763d97e20
X <xac923@outlook.com>
2022-01-17 16:58:55 -0800

    added some packages

--- server.js
+++ server.js
@@ -23,6 +23,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false })); //this renders create post.
 app.use(express.static('public'));
 app.use(methodOverride("_method"));
+
+
 app.use((req, res, next) => {
     console.log(req.url, req.method);
     next();
